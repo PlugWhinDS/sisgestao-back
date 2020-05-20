64863348d67147c2061ec4936e78aa96a8a4000d
Cristian Madeira de Souza Pereira <cristianpereira@tic.ufrj.br>
2020-05-20 18:33:39 -0300

    Add express and sequelize dockerized.

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').configure()
+module.exports=
+{
+  "development": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
+    
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+}
---
+++ docker/scripts/start-app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo 'Running package...'
+npm install --save
+
+echo 'Running migration...'
+npx sequelize-cli db:migration
+
+echo 'Starting app...'
+npm start
